24618bfb6b549ad6208ae7061d375792ad8986e3
alifprihantoro <alifmuryp@gmail.com>
2023-01-19 16:50:16 +0700

    feat: change react to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from '@astrojs/react'
-
-// https://astro.build/config
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-})
-
+  integrations: [tailwind(), preact({
+    compat: true
+  })]
+});
