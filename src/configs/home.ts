1248979cf78f957e7c678b5c3f96d7cabb0b5d0a
alifprihantoro <alifmuryp@gmail.com>
2023-01-19 13:26:04 +0700

    feat: add layouts and alias path tsconf

---
+++ src/configs/home.ts
@@ -0,0 +1,7 @@
+export const TITLE = 'Muryp'
+export const LINK = 'https://muryp.my.id/'
+export const IMG = ''
+export const DESCRIPTION = ''
+export const SEO = {
+  TITLE, LINK, IMG, DESCRIPTION,
+}
---
+++ src/layouts/Main.tsx
@@ -0,0 +1,21 @@
+import type { seo } from '@Mytypes/Seo'
+import Seo from './Seo'
+
+interface props {
+  SEO: seo
+  children: string | number | React.ReactElement | React.ReactElement[] | React.ReactFragment;
+}
+export default function Main(props: props) {
+  const { SEO, children } = props
+  return (
+    <html lang='en'>
+      <head>
+        <Seo seo={SEO} />
+      </head>
+      <body>
+        {children}
+        {/*script */}
+      </body>
+    </html>
+  )
+}
