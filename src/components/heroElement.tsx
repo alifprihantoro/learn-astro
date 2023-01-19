d76140af2cd510ef1c519ab91263d0cdc2b22e4c
alifprihantoro <alifmuryp@gmail.com>
2023-01-19 20:41:24 +0700

    feat: add structure file home

---
+++ src/components/heroElement.tsx
@@ -0,0 +1,3 @@
+export default function HeroElement(){
+  return <div>hello from hero element</div>
+}
---
+++ src/layouts/home/content.tsx
@@ -0,0 +1,3 @@
+export default function ContentHome(){
+  return <div>hello from content home</div>
+}
