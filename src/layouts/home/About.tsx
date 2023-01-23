a4a417ab3fca87e342f94700c44e66ca6481efa8
alifprihantoro <alifmuryp@gmail.com>
2023-01-24 02:32:56 +0700

    feat: mv file, add file abstract

---
+++ src/layouts/home/About.tsx
@@ -0,0 +1,8 @@
+export default function AboutHome(){
+  return (
+    <div>
+      <h2>Whats is MuryP?</h2>
+      <h2>Roadmap</h2>
+    </div>
+  )
+}
---
+++ src/layouts/home/Contact.tsx
@@ -0,0 +1,8 @@
+export default function ContactHome() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      {/* btn contact */}
+    </div>
+  )
+}
