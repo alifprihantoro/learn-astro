314e9d74aea4d6afcdaf149122153bb09857f654
alifprihantoro <alifmuryp@gmail.com>
2023-01-24 01:24:36 +0700

    refactor: add configs project

--- src/components/heroElement.tsx
+++ src/components/heroElement.tsx
@@ -1,10 +1,13 @@
+import HERO_EL from '@configs/home/HERO_EL'
+
 export default function HeroElement() {
+  const { title, paragraph } = HERO_EL
   return (
     <div className="hero min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <h2 className="text-5xl font-bold">{title}</h2>
+          <p className="py-6">{paragraph}</p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
