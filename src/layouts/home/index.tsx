5d4444596fef5c81415a3b54fabcfa13aa9a2159
alifprihantoro <alifmuryp@gmail.com>
2023-01-25 20:21:12 +0700

    feat: add post wrapper and merger nav and footer

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,6 +1,4 @@
-import Nav from '@components/nav'
 import HeroElement from '@components/heroElement'
-import Footer from '@components/footer'
 import ListCardImg from '@components/ListCardImg'
 import project from '@Mytypes/project'
 import ContactHome from './Contact'
@@ -9,7 +7,6 @@ import AboutHome from './About'
 export default function Home() {
   return (
     <>
-      <Nav />
       <HeroElement />
       <div className="md:flex">
         <AboutHome />
@@ -21,7 +18,6 @@ export default function Home() {
           <ListCardImg content={project} />
         </div>
       </main>
-      <Footer />
     </>
   )
 }
