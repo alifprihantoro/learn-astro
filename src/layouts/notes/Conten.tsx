6e974eeb5ab32fa6f7829be99634a6e80e5f42b8
alifprihantoro <alifmuryp@gmail.com>
2023-01-25 21:01:31 +0700

    feat: notes list and content wrapper

--- src/layouts/notes/Conten.tsx
+++ src/layouts/notes/Conten.tsx
@@ -1,7 +1,3 @@
-import Nav from '@components/nav'
-import Footer from '@components/footer'
-import { NAV_LIST } from '@configs/notes/nav'
-
 interface props {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   children: any
@@ -9,11 +5,9 @@ interface props {
 export default function ContentNote({ children }: props) {
   return (
     <>
-      <Nav LIST={NAV_LIST} />
       <main>
         {children}
       </main>
-      <Footer />
     </>
   )
 }
