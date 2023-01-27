fda7159a6c224bfc54f7a0e84c84a608d4416a31
alifprihantoro <alifmuryp@gmail.com>
2023-01-27 10:16:07 +0700

    feat: sort by date blogs
    - types props
    - split props

---
+++ src/types/blogProps.ts
@@ -0,0 +1,30 @@
+import type { AstroComponentFactory } from 'astro/dist/runtime/server'
+
+export interface author {
+  name: string
+  uri: string
+}
+export interface posts {
+  title: string
+  slug: string | undefined | boolean
+  date: string
+  lasmod: string
+  draft: boolean
+  show_comments: boolean
+  type: string
+  tags: string[]
+  image: string
+  thumbnail: string
+  description: string
+  author: author
+}
+
+export interface astro {
+  TITLE: string
+  Content: AstroComponentFactory
+  slug: string
+  DESCRIPTION: string
+  AUTHOR: author
+  DATE: string
+  TAGS: string[]
+}
---
+++ src/utils/getPropsBlogs.ts
@@ -0,0 +1,33 @@
+import getBlogMd from '@utils/getBlogMd'
+import type { MarkdownInstance } from 'astro'
+import type { posts } from '@Mytypes/blogProps'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function getPropsBlogs(posts: MarkdownInstance<Record<string, any>>[]) {
+  posts.sort((a, b) => {
+    const A_DATE = new Date(a.frontmatter.date).valueOf()
+    const B_DATE = new Date(b.frontmatter.date).valueOf()
+    return B_DATE - A_DATE
+  })
+  return posts.map((post) => {
+    const frontmatter = post.frontmatter as posts
+    const TITLE = frontmatter.title
+    let slug = frontmatter.slug
+    slug =
+      slug === undefined || slug === false
+        ? getBlogMd(post.file)
+        : frontmatter.slug
+    const DESCRIPTION = frontmatter.description
+    const Content = post.Content
+    let AUTHOR = frontmatter.author
+    AUTHOR =
+      AUTHOR === undefined ? { name: 'Alief Prihantoro', uri: '#' } : AUTHOR
+    const DATE = frontmatter.date
+    const TAGS = frontmatter.tags
+    const LASTMOD = frontmatter.lasmod
+    return {
+      params: { slug },
+      props: { TITLE, Content, slug, DESCRIPTION, AUTHOR, DATE, TAGS, LASTMOD },
+    }
+  })
+}
