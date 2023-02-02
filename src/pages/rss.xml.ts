a09c836533f787ea84e416ae6978633e864a013d
alifprihantoro <alifmuryp@gmail.com>
2023-02-02 19:14:54 +0700

    fix: rss js to ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,14 @@
 import rss from '@astrojs/rss'
 import getPropsBlogs from '@utils/getPropsBlogs'
+import type { MarkdownInstance } from 'astro'
 
-export async function get(context) {
-  // const blog = await getCollection('blog')
+interface Context {
+  site: string
+}
+export async function get(context: Context) {
   const postImportResult = import.meta.glob('../content/blog/**/*.md', { eager: true })
-  const posts = Object.values(postImportResult)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const posts = Object.values(postImportResult) as MarkdownInstance<Record<string, any>>[]
   const POST_SORT = getPropsBlogs(posts)
   const ITEMS = POST_SORT.map(({ props }) => {
     const { TITLE, DATE, SLUG, DESCRIPTION } = props
