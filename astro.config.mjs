602660aa6d88f44ad7a356a0bd05cdb0638898ad
alifprihantoro <alifmuryp@gmail.com>
2023-02-02 15:46:16 +0700

    feat: add rss and robot.txt and minnimal config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
+import preact from '@astrojs/preact';
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://muryp.my.id',
   integrations: [preact({
-    compat: true,
-  }),
-  tailwind({
+    compat: true
+  }), tailwind({
     config: {
       applyBaseStyles: true,
-      path: './tailwind.config.cjs',
-    },
-  })],
-})
+      path: './tailwind.config.cjs'
+    }
+  }), robotsTxt()]
+});
