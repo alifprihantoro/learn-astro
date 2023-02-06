5e04d35e21b292b3b1923682287cc0fc39efc4ef
alifprihantoro <alifmuryp@gmail.com>
2023-02-06 19:11:33 +0700

    feat: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
-import robotsTxt from 'astro-robots-txt'
+import preact from '@astrojs/preact';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import turbolinks from '@astrojs/turbolinks'
+import turbolinks from '@astrojs/turbolinks';
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://muryp.my.id',
   integrations: [preact({
-    compat: true,
+    compat: true
   }), tailwind({
     config: {
       applyBaseStyles: true,
-      path: './tailwind.config.cjs',
-    },
-  }), robotsTxt(), sitemap(), turbolinks()],
-})
+      path: './tailwind.config.cjs'
+    }
+  }), robotsTxt(), sitemap(), turbolinks(), partytown()]
+});
