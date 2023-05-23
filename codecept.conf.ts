c8970833082b1892f5014cbc7c9e2ceff8ac7a67
alifprihantoro <alifprihantoro@gmail.com>
2023-05-23 13:26:14 +0700

    test: change config browser

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -1,23 +1,24 @@
-import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+import { setHeadlessWhen, setCommonPlugins, CodeceptJS } from '@codeceptjs/configure'
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+setHeadlessWhen(process.env.HEADLESS)
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
+setCommonPlugins()
 
 export const config: CodeceptJS.MainConfig = {
   tests: './e2e/*.spec.ts',
   output: './output',
   helpers: {
     Playwright: {
-      url: 'localhost:3000',
+      url: 'http://localhost:3000',
       show: true,
-      browser: 'chromium'
-    }
+      browser: 'chromium',
+      timeout: 90000,
+    },
   },
   include: {
-    I: './steps_file'
+    I: './steps_file',
   },
-  name: 'muryp'
+  name: 'muryp',
 }
